b1b4901d19178bf2e56088f4cec18cae286daa4e
Adrián de la Cruz <adrian.gonzalez.tic@gmail.com>
2022-09-01 13:42:50 -0500

    segunda carga

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+# Función base
 print("Hola perro del mal")
 print("algo")
 print("algo")
-print("algo")
-print("algo")
-print("algo")
-print("algo")
+
+a = 12 * 12
+
+
+def main():
+    algo = [1,2,3,4]
+    for i in algo:
+        print(algo)
